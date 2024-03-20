592140f9fba2b41a32f1fee0b72239c2c740a77e
ogola <ogolajeremy601@gmail.com>
2024-03-20 13:44:17 +0300

    adds-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "mark"
+
+function upperCaseCustomerName(){
+    customerName = "BOB"
+
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Jemas"
+
+}
